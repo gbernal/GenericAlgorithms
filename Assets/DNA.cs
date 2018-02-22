e654ab50bef7bba1af34f15f706be0fc7d40dfe6
Guillermo Bernal <gbernal@mit.edu>
2018-02-21 22:28:52 -0500

    added DNA scipt

--- Assets/DNA.cs
+++ Assets/DNA.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DNA : MonoBehaviour {
+    //gene for color
+    public float r;
+    public float g;
+    public float b;
+
+    bool dead = false;
+
 
 	// Use this for initialization
 	void Start () {
